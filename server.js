5e50d34a79ac40167c7286eb34318a62d8f6fe01
Norman <ubotirov77@gmail.com>
2026-05-01 16:04:31 +0900

    feat: building simple backend server on node.js

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 // Node.js
 // python django fastapi
+const express = require('express')
+const app = express()
+
+// traditional get api
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/rest', (req, res) => {
+    res.json({name: 'Norman', age: 23, group: 'MIT'})
+  })
+
+app.listen(3000, () => {
+  console.log(`Backend Server is running on port 3000`)
+})
