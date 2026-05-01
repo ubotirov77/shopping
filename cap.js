7b60e3a16781987c16d83c0e34f259d2f0d712bd
Norman <ubotirov77@gmail.com>
2026-05-01 16:35:12 +0900

    feat: advantages of callback and promise

---
+++ cap.js
@@ -0,0 +1,25 @@
+/* Asynchronous function : Async vs promise
+define           call
+callback       callback 
+async/await    then/catch
+promise        then/catch
+*/
+// define 
+function division(a, b){
+    return new Promise((resolve, reject) => {
+    if(b === 0){
+        reject("Not devided by zero");
+    } else{
+        setInterval(() => {
+            resolve(a % b);
+        }, 5000);
+    }
+});
+}
+// call
+division(10, 3).then(data => {
+    console.log("Result division:", data);
+    console.log(".................");
+}).catch(err => {
+    console.log("Error division:", err);
+});
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-// Node.js
-// python django fastapi
+
 const express = require('express')
 const app = express()
 
 // traditional get api
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+   
+    res.send('<h1>Hello World!</h1>')
 })
-
+// rest api
 app.get('/rest', (req, res) => {
+    
     res.json({name: 'Norman', age: 23, group: 'MIT'})
   })
 
